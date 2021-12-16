118300e60de54243a8d51f8cc38b5f72341880e8
Shogo <sho5tkmr0802@gmail.com>
2021-12-16 12:40:52 +0900

    Fix: 使用する色を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+            100: '#fffaf0',
+            200: '#feebc8',
+            300: '#fbd38d',
+            400: '#f6ad55',
+            500: '#ed8936',
+            600: '#dd6b20',
+            700: '#c05621',
+            800: '#9c4221',
+            900: '#7b341e'
+        },
+      },
+    },
   },
   variants: {
     extend: {},
