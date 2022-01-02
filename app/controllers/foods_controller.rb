19aa50ddddea63e2fe5c02b171925b7a9ea4a39e
Shogo <sho5tkmr0802@gmail.com>
2022-01-02 20:36:24 +0900

    Fix: フォームからのレシピ投稿実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,7 +6,7 @@ class FoodsController < ApplicationController
   def create
     @food = current_user.foods.build(food_params)
     if @food.save
-      redirect_to foods_path, success: t('.secucess')
+      redirect_to root_path, success: t('.success')
     else
       flash[:danger] = t('.fail')
       render :new
