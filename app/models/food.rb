429a19833636cf96ca398e6776c145d86b17e4ed
Shogo <sho5tkmr0802@gmail.com>
2022-01-10 13:39:39 +0900

    Fix: Foodのエラーメッセージが表示されるように修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -9,6 +9,16 @@ class Food < ApplicationRecord
   # tagとはfood_tagsを介してつながる
   has_many :tags, through: :food_tags
 
+  with_options presence: true do
+    validates :name
+    # 作り方の文字数を制限
+    validates :recipe, length: { maximum: 528 }
+    validates :cooking_time
+    validates :cooking_time_unit
+    # 何人前か上限と下限を指定（整数で1〜10）
+    validates :serving, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  end
+
   # 調理時間の単位
   enum cooking_time_unit: { seconds: 0, minutes: 1, hours: 2 }
 
