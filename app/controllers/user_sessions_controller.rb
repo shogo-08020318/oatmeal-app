b8a2b221d9b4e6e8265247dde5be259b4931e55f
Shogo <sho5tkmr0802@gmail.com>
2022-02-05 10:52:50 +0900

    Fix: require_login追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,11 +1,13 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login
+
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to root_path, success: t('.success')
+      redirect_back_or_to foods_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new
@@ -14,6 +16,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path, success: t('.success')
+    redirect_to login_path, success: t('.success')
   end
 end
