d7bf5bc7d33a03d459c2cf5e537d53f3fbf38aa7
Shogo <sho5tkmr0802@gmail.com>
2022-02-10 15:37:20 +0900

    Fix: マクロ栄養素とカロリーを1食分当たりの表示に変更

--- app/models/nutrition.rb
+++ app/models/nutrition.rb
@@ -9,10 +9,23 @@ class Nutrition < ApplicationRecord
     validates :fat
   end
 
-  def calorie_calculates
-    macro_calories = [carbo * 4, protein * 4, fat * 9]
+  def calorie_calculates(serving)
+    macro_calories = [
+      carbo * 4 / serving,
+      protein * 4 / serving,
+      fat * 9 / serving
+    ]
+    self.calories /= serving
     results = []
     macro_calories.each { |calorie| results << (calorie / calories * 100).round(1) }
     results << calories.to_i
+  end
+
+  def nutrition_calculates(serving)
+    [
+      self.carbo /= serving,
+      self.protein /= serving,
+      self.fat /= serving
+    ]
   end
 end
