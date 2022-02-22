926a6e23667ecba4da5331c9dfe3f3bbcb2e8745
Shogo <sho5tkmr0802@gmail.com>
2022-02-22 16:25:00 +0900

    Fix: カロリー、炭水化物、たんぱく質、脂質での検索機能実装

--- app/form/food_search_form.rb
+++ app/form/food_search_form.rb
@@ -1,19 +1,44 @@
 class FoodSearchForm
   include ActiveModel::Model
 
-  attr_accessor :name, :ingredient_name, :not_ingredient_name, :food_tags
+  attr_accessor :name, :ingredient_name, :not_ingredient_name, :over_calories, :under_calories, :over_carbo, :under_carbo, :over_protein, :under_protein, :over_fat, :under_fat, :food_tags
 
   def search
     foods = Food.distinct.joins(:ingredients, :nutrition)
 
     # レシピ名と材料名から検索
-    foods = foods.in_name(name).or(foods.in_ingredients(name))
+    foods = foods.in_name(name).or(foods.in_ingredients(name)) if name.present?
 
     # 材料名のみで検索
-    foods = foods.in_ingredients(ingredient_name)
+    foods = foods.in_ingredients(ingredient_name) if ingredient_name.present?
 
     # 特定の材料を除外
-    foods.not_ingredients(not_ingredient_name) if not_ingredient_name.present?
+    foods = foods.not_ingredients(not_ingredient_name) if not_ingredient_name.present?
+
+    # カロリー検索（以上）
+    foods = foods.over_calories(over_calories) if over_calories.present?
+
+    # カロリー検索（以下）
+    foods = foods.under_calories(under_calories) if under_calories.present?
+
+    # 炭水化物検索（以上）
+    foods = foods.over_carbo(over_carbo) if over_carbo.present?
+
+    # 炭水化物検索（以下）
+    foods = foods.under_carbo(under_carbo) if under_carbo.present?
+
+    # たんぱく質検索（以上）
+    foods = foods.over_protein(over_protein) if over_protein.present?
+
+    # たんぱく質検索（以下）
+    foods = foods.under_protein(under_protein) if under_protein.present?
+
+    # 脂質検索（以上）
+    foods = foods.over_fat(over_fat) if over_fat.present?
+
+    # 脂質検索（以下）
+    foods = foods.under_fat(under_fat) if under_fat.present?
+
     foods
   end
 end
