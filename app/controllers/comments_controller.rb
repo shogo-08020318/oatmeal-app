ad7af555992551378ec2222b525dc8b7901e7555
Shogo <sho5tkmr0802@gmail.com>
2022-03-13 10:41:26 +0900

    Add: コメント機能のAjax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      redirect_to food_path(@comment.food.uuid), success: t('.success')
-    else
-      redirect_to food_path(@comment.food.uuid), danger: t('.fail')
-    end
+    @comment.save
   end
 
   def destroy
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
-    redirect_to food_path(@comment.food.uuid), success: t('.success')
   end
 
   private
